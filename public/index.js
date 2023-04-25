b9f1223929146c45bd04c77b6a5de0fb1be086a0
JonsCodes <johnalyashai@me.com>
2023-04-24 20:38:28 -0700

    completed

--- public/index.js
+++ public/index.js
@@ -1,9 +1,14 @@
-function main(){
+async function main(){
     const countContainer = document.querySelector('#count-container');
     const incrementButton = document.querySelector('#increment-button');
     const decrementButton = document.querySelector('#decrement-button');
 
-    let countValue = 0;
+  const response = await fetch('http://127.0.0.1:9001/counter');
+  console.log(response);
+  const responseObject = await response.json();
+  console.log(responseObject);
+  let countValue = responseObject.value;
+
 
     function increment(){
         countValue++;
